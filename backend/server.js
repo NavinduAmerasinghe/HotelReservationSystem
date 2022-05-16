af3643aa6be7be1a59de1a6a2a69be139b232000
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2022-05-16 18:44:23 +0530

    Folder structure created

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 9000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected Successfully..");
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
